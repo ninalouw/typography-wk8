c3173f40e898bd643bdaf10727255590624bbadc
Nina Louw <ninalouw@mac.com>
2017-10-27 11:42:23 -0700

    format images, finish index page

--- index.php
+++ index.php
@@ -1,4 +1,150 @@
 <!-- php header -->
 <?php include "partials/header.php"?>
+
+<div class="container">
+    <div class="row hero">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h1>Only the best fonts for the most creative people</h1>
+            <img class='img-fluid img-responsive' src='img/font-slider/wolpe-banner.png' alt='font-banner'>
+            <div class="banner">
+                <h3>Over 3000 satisfied clients!</h3>
+                <button class="btn btn-primary" href="portfolio.php">View Portfolio</button>
+            </div>
+        </div>
+    </div>
+        <!-- start of product cards -->
+    <h2 class="text-center">Fontboy Recommends</h2>
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 card-mobile">
+            <div class="card">
+            <img class="card-img-top" src="img/font-type/square/mona.png" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Mona</h4>
+                <p class="card-text text-muted">Script font</p>
+                <p class="card-text">$50</p>
+                <a href="portfolio.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 card-mobile">
+            <div class="card">
+            <img class="card-img-top" src="img/font-type/square/peomy.jpg" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Peomy</h4>
+               <p class="card-text text-muted">Script Font</p>
+                <p class="card-text">$50</p>
+                <a href="portfolio.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 card-mobile">
+            <div class="card">
+            <img class="card-img-top" src="img/font-type/square/sunn-line.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Sunn Line</h4>
+               <p class="card-text text-muted">Sans-serif Font</p>
+                <p class="card-text">$88</p>
+                <a href="portfolio.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 card-mobile">
+            <div class="card">
+            <img class="card-img-top" src="img/font-type/square/fairwater.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Fairwater</h4>
+                <p class="card-text text-muted">Serif Font</p>
+                <p class="card-text">$54</p>
+                <a href="products.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- end of card section -->
+    <!-- Test my type section -->
+    <div class="row hero">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h2 class="text-center"> Test My Type</h2>
+            <section class="filters">
+                <div class="filters-categories">
+                    <div class="filters-toggles">
+                        <select>
+                            <option value="bodoni">Bodoni</option>
+                        </select>
+                        <span class="filters-toggle active" data-font="bold">Bold</span>
+                        <span class="filters-toggle active" data-font="italic">Italic</span>
+                        <span class="filters-toggle active" data-font="uppercase">Upcase</span>
+                        <span class="filters-toggle active" data-font="lowercase">Lowcase</span>
+                        <span class="filters-toggle active" data-font="size">Size</span>
+                    </div>
+                </div>
+            </section>
+            <form>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Type something...</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- second row of cards -->
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 card-mobile">
+            <div class="card">
+            <img class="card-img-top" src="img/font-type/square/plume.jpg" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Plume</h4>
+                <p class="card-text text-muted">Script font</p>
+                <p class="card-text">$50</p>
+                <a href="portfolio.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 card-mobile">
+            <div class="card">
+            <img class="card-img-top" src="img/font-type/square/vintii.jpg" alt="Card image">
+            <div class="card-body">
+                <h4 class="card-title">Vintii</h4>
+               <p class="card-text text-muted">Script Font</p>
+                <p class="card-text">$50</p>
+                <a href="portfolio.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 card-mobile">
+            <div class="card">
+            <img class="card-img-top" src="img/font-type/square/handelson.png" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Handelson</h4>
+               <p class="card-text text-muted">Serif Font</p>
+                <p class="card-text">$88</p>
+                <a href="portfolio.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 card-mobile">
+            <div class="card">
+            <img class="card-img-top" src="img/font-type/square/geogro-slab.png" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Geogro Slab</h4>
+                <p class="card-text text-muted">Serif Font</p>
+                <p class="card-text">$54</p>
+                <a href="products.php" class="btn btn-primary">View</a>
+            </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- end of card section -->
+
+<!-- end of container -->
+</div>
 <!-- php footer -->
 <?php include "partials/footer.php"?>
